2253a378d3c43f6980c5931f6e8654b9f96dc01c
jaeryo2357 <jaeryo2357@naver.com>
2019-03-01 15:39:12 +0900

    데이터베이스와 모델

--- app/Author.php
+++ app/Author.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    public $timestamps=false;
+    protected $fillable = ['email','password'];
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+   public $timestamps = false;
+   protected $fillable = ['email','password'];
 }
