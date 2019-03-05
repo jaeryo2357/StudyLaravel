cac2e6a5904a8c3324692a62676285e855bf13a4
jaeryo2357 <jaeryo2357@naver.com>
2019-03-05 17:10:14 +0900

    사용자 인증

--- config/mail.php
+++ config/mail.php
@@ -56,9 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+        'address' => 'john@example.com',
+        'name' => 'Admin'],
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,34 @@ Route::get('/binding',function(){
 });
 
 Route::resource('article','ArticlesController');
+
+Route::get('auth/login',function(){
+$credentials=[
+	'email'=>'john@example.com',
+	'password'=>'password'
+];
+if(! auth()->attempt($credentials)){
+return '로그인 정보가 정확하지 않습니다.';
+}
+
+return redirect('protected');
+});
+
+Route::get('protected',function(){
+	dump(session()->all());
+
+	if(! auth()->check()){
+	return '누구세요';
+	}
+	return '어서오세요'.auth()->user()->name;
+});
+
+Route::get('auth/logout',function(){
+auth()->logout();
+return '또 봐요~';
+
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
